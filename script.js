672016c527c2417bea2c8f16295cd3f910f97718
L. K <LK00100100@gmail.com>
2022-04-14 00:12:45 -0400

    scrolling works

--- script.js
+++ script.js
@@ -1,4 +1,47 @@
-let scrollTo = function () {
-  console.log("clicked");
-};
+console.log("script.js loaded");
 
+let isScrolling = false;  //if animated and scrolling
+
+/**
+ * Scrolls from current scroll position to targetId.
+ * Does not work while already doing animated scrolling.
+ * @param {String} targetId Assumed valid id. No need to put "#". Ex: portfolio
+ */
+function animateScrollTo(targetId) {
+  if (isScrolling) {
+    console.log("already scrolling..");
+    return;
+  }
+
+  isScrolling = true;
+
+  console.log(`scrolling to ${targetId}`);
+
+  let currentScrollPos = window.pageYOffset || document.documentElement.scrollTop;
+
+  let targetScrollPos = $("#" + targetId).offset().top;
+
+  let distance = Math.abs(targetScrollPos - currentScrollPos);
+  let numSlices = 100;
+  let distSlice = distance / numSlices;
+
+  //we need to go up. instead of down.
+  if (targetScrollPos < currentScrollPos)
+    distSlice = -distSlice;
+
+  //scroll to target, in numSlices
+  let scrollInterval = setInterval(function () {
+    currentScrollPos += distSlice;
+    $("html").scrollTop(currentScrollPos);
+
+    //no more slices left
+    if (numSlices == 0) {
+      $("html").scrollTop(targetScrollPos);
+      clearInterval(scrollInterval);
+      isScrolling = false;
+    }
+
+    numSlices--;
+  }, 5);
+
+};
